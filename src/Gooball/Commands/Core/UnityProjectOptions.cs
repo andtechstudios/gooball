59d11bc317ee53e2b3ba6699e788ef533abac09e
Andrew <andrewmjordan@users.noreply.github.com>
2021-11-15 13:50:45 -0800

    begin refactor of follow option

--- src/Gooball/Commands/Core/UnityProjectOptions.cs
+++ src/Gooball/Commands/Core/UnityProjectOptions.cs
@@ -7,7 +7,9 @@ namespace Andtech.Gooball
 	{
 		[Value(0, Required = false, Default = "./", HelpText = "The path to the Unity project.")]
 		public string ProjectPath { get; set; }
-		[Option('f', "follow", HelpText = "Output appended data as the Unity log grows.")]
+		[Option('f', "follow", HelpText = "Output appended data as the Unity log grows. (same as dumplog)")]
 		public bool Follow { get; set; }
+		[Option("dumplog", HelpText = "Output appended data as the Unity log grows. (same as follow)")]
+		public bool DumpLog { get; set; }
 	}
 }
